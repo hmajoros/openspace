f7b24964d9d8d42cdba3aa4b7bb433cf44907dae
Kyle Frost <kylefrost@comcast.net>
2015-01-17 03:56:15 -0500

    Add DB connect to home views.py.......nigga

--- openspace/home/views.py
+++ openspace/home/views.py
@@ -1,11 +1,23 @@
 from flask import render_template, Blueprint
+import os
+import mysql.connector
+from mysql.connector import errorcode
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return render_template('home.html', title='home')
+    connectToDB()
+    return render_template('home.html', title='Home')
 
+def connectToDB():
+    config = {
+        'user': os.environ['DB_USER'],
+        'password': os.environ['DB_PASSWORD'],
+        'host': os.environ['DB_HOST'],
+        'database': os.environ['DB_NAME'],
+        'raise_on_warnings': True,
+    }
 
-
-
+    cnx = mysql.connector.connect(**config)
+    cnx.close()
