5ff81ea2b0f199a9b4a0142e210b6238093a8480
Henry Majoros <majoros.henry@gmail.com>
2015-01-18 02:37:50 -0500

    most database and forms work now. getting close

--- openspace/dashboard/views.py
+++ openspace/dashboard/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, g, session
+from flask import render_template, Blueprint, g, session, redirect, url_for, request
 from flask.ext.login import current_user
 
 import os
@@ -18,6 +18,30 @@ def index():
                             user=current_user, 
                             sites=sites)
 
+@dashboard.route('/create_page', methods=['GET', 'POST'])
+def create_page():
+    if request.method == 'POST':
+        db_user = os.environ['DB_USER']
+        db_pass = os.environ['DB_PASSWORD']
+        db_host = os.environ['DB_HOST']
+        db_name = os.environ['DB_NAME']
+
+        conn = mysql.connector.connect(user=db_user, password=db_pass, host=db_host, database=db_name)
+        cursor = conn.cursor()
+        query = ("INSERT INTO pages (user_id, template_id, page_name) VALUES (%s, %s, %s)")
+        values = (current_user.get_id(), request.form['template_id'], request.form['page_name'])
+
+        cursor.execute(query, values)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return redirect(url_for('dashboard.index'))
+    return redirect(url_for('home.index'))
+
+
+
 
 def getSitesFor(user_id):
     db_user = os.environ['DB_USER']
@@ -27,7 +51,7 @@ def getSitesFor(user_id):
 
     conn = mysql.connector.connect(user=db_user, password=db_pass, host=db_host, database=db_name)
     cursor = conn.cursor()
-    query = ("SELECT t.template_name, p.page_id FROM templates t, pages p, users u "
+    query = ("SELECT t.template_name, p.page_name FROM templates t, pages p, users u "
              "WHERE u.user_id = %(user_id)s "
              "AND u.user_id = p.user_id "
              "AND p.template_id = t.template_id")
@@ -38,17 +62,17 @@ def getSitesFor(user_id):
 
     row = cursor.fetchone()
     while row is not None:
-        site = Site(row[1], row[0])
+        site = Site(row[0], row[1])
         sites.append(site)
         row = cursor.fetchone()
     return sites
 
 class Site(object):
-    name = ""
-    href = ""
+    t_name = ""
+    p_name = ""
     
-    def __init__(self, href, name):
-        self.href = href
-        self.name = name
+    def __init__(self, t_name, p_name):
+        self.t_name = t_name
+        self.p_name = p_name
 
 
--- openspace/upload/views.py
+++ openspace/upload/views.py
@@ -1,5 +1,10 @@
-from flask import render_template, Blueprint, g, session
-from flask.ext.login import current_user
+from flask import render_template, Blueprint, g, session, request, redirect, url_for
+from flask.ext.login import current_user, login_required
+
+import os
+import mysql.connector
+import mysql
+
 
 upload = Blueprint('upload', __name__, url_prefix='/upload')
 
@@ -8,5 +13,28 @@ def before_request():
     g.user = current_user
 
 @upload.route('/')
+@login_required
 def index():
     return render_template('upload.html', title='Upload', user=current_user)
+
+@upload.route('/template', methods=['GET', 'POST'])
+def template():
+    if request.method == 'POST':
+        db_user = os.environ['DB_USER']
+        db_pass = os.environ['DB_PASSWORD']
+        db_host = os.environ['DB_HOST']
+        db_name = os.environ['DB_NAME']
+
+        conn = mysql.connector.connect(user=db_user, password=db_pass, host=db_host, database=db_name)
+        cursor = conn.cursor()
+
+        query = ("INSERT INTO templates (template_content, template_name, author_id) VALUES (%s, %s, %s)")
+        values = (request.form['content'], request.form['template_name'], current_user.get_id())
+        cursor.execute(query, values)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return redirect(url_for('home.index'))
+    return redirect(url_for('upload.index'))
